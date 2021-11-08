ac4572c7de7876a5a1fbc04ad3f30ad451972420
BlackNoizE404 <blacknoize404@gmail.com>
2021-11-07 20:02:27 -0500

    First Commit

---
+++ pages/inspire.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+      <blockquote class="blockquote">
+        &#8220;Primero, resuelve el problema. Luego, escribe el código.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
